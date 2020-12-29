661fe430d671894eb9dc51ee1d2a3a90cced9015
lawrencechoy123 <74360010+lawrencechoy123@users.noreply.github.com>
2020-12-29 16:24:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,28 +5,29 @@ import os
 import glob
 from datetime import datetime
 
-#create a list to get the images from our folder automatically
-#generate encoding for it and find it in our webcam
+#create list with images from our folder and generate image encodings
 image_folder_path = 'data/*.jpg'
 images, classNames, ID = [], [], []
-#use class names and import the imgs 1 by 1  cl = name of our image
-#(os.path.splitext(cl)[0] will give us the file name without .jpg
+
+#import images 1 by 1  from classNames
 for cl in glob.glob(image_folder_path):
     curImg = cv2.imread(cl)
     images.append(curImg)
+    #(os.path.splitext(cl)[0] will give us the file name without .jpg
     classNames.append(os.path.splitext(cl)[0])
 
-#to display name and student ID from image name
+#to obtain and display name and student ID 
 for i in classNames:
     v = i
     splitedName = v.split('_')
     Name = splitedName[0]
     ID = splitedName[1]
 
-#once we import images, we find the encodings for each of them
+#finding the encodings for each of image
 def findEncoding(images):
     encodeList = []
     for img in images:
+        #convert to colours that the program recognises 
         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
@@ -35,6 +36,7 @@ def findEncoding(images):
 encodeListKnown = findEncoding(images)
 print('Encoding Complete')
 
+#to initiate webcam
 cap = cv2.VideoCapture(0)
 
 #to mark attendance with name, date, time and student ID
@@ -56,9 +58,8 @@ def markAttendance(name,ID):
 
 while True:
     success, img = cap.read()
-    #since we are doing this in real time, we will reduce the image size to speed up the process
+    #speed up process by reducing image size as it is in real time
     #(img) = send in image , (0,0) = pixels size , 0.25, 0.25 = scale (1/4 of size)
-    #convert colour afterwards
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
@@ -91,6 +92,7 @@ while True:
             y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
             cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            #putText labels live images with name and ID on webcam
             cv2.putText(img,Person_Name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             cv2.putText(img,STUDENT_ID,(x1+6,y2-178),cv2.FONT_ITALIC,0.5,(255,255,255),2)
             markAttendance(Person_Name,STUDENT_ID)
